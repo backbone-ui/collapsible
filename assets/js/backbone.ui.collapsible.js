d65bf28f09d8651fca73fffdef4b608f47360f22
Lyndel Thomas <lyndelt@gmail.com>
2013-04-15 00:20:13 -0700

    initial commit

---
+++ assets/js/backbone.ui.collapsible.js
@@ -0,0 +1,27 @@
+// Backbone.js Content extension
+//
+// Created by: Lyndel Thomas (@ryndel)
+// Source: https://github.com/backbone-ui/collapsible
+//
+// Licensed under the MIT license: 
+// http://makesites.org/licenses/MIT
+
+(function(_, Backbone) {
+	
+	// fallbacks
+	if( _.isUndefined( Backbone.UI ) ) Backbone.UI = {};
+	// Support backbone app (if available)
+	var View = ( typeof APP != "undefined" && !_.isUndefined( APP.View) ) ? APP.View : Backbone.View;
+	
+	Backbone.UI.Collapsible = View.extend({
+		
+		events: {
+			"click h2": "toggle",
+		},
+		
+		toggle: function( e ) {
+			$(e.target).parent("section").toggleClass('active');
+		} 
+	});
+	
+})(this._, this.Backbone);
